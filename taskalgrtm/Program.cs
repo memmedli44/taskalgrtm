36a0f3f58609e91d49efd1d2c1eca4b15bb05498
ASUS <ASUS@WIN-NOPEK8RRCHF>
2023-02-21 11:03:20 +0400

    Добавьте файлы проекта.

---
+++ taskalgrtm/Program.cs
@@ -0,0 +1,371 @@
+﻿namespace taskalgrtm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1
+
+            //Console.WriteLine("ededi daxil edin ");
+            //int i = int.Parse(Console.ReadLine());
+            //int reqemsayi = (int)Math.Log10(i) + 1;
+
+
+
+            //if (reqemsayi == 4)
+            //{
+            //    int result = 7 * (int)Math.Pow(10, reqemsayi + 1) + i * 10 + 8;
+            //    Console.WriteLine(result);
+
+
+
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("4  reqemli eded deyil");
+
+            //}
+
+            #endregion
+
+
+            #region task 2 
+
+            //Console.WriteLine("ededi daxil edin ");
+            //int i = int.Parse(Console.ReadLine());
+            //int reqemsayi = (int)Math.Log10(i) + 1;
+
+            //if (reqemsayi == 3)
+            //{
+            //    int result = i * (int)Math.Pow(10, reqemsayi) + i;
+            //    Console.WriteLine(result);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("eded 3 reqemli deyil");
+            //}
+            #endregion
+
+            #region task 3
+
+            //Console.WriteLine("ededi daxil edin ");
+            //int i =int.Parse(Console.ReadLine());
+            //int say = (int)Math.Log10(i) + 1;
+
+            //if(say == 5)
+            //{
+            //    double a= i*0.18;
+            //    double b = a * 0.03;
+            //    Console.WriteLine($"{a}  {b}");
+
+
+
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("eded 5 reqemli deyil");
+            //}
+
+
+            #endregion
+
+            #region task 4
+
+            //Console.WriteLine("ededi daxil edin ");
+            //int i = int.Parse(Console.ReadLine());
+            //int say = (int)Math.Log10(i) + 1;
+
+            //if(say == 3) 
+            //{
+            //    int a = i * 10 + 7;
+            //    double b = a * 0.07;
+            //    Console.WriteLine(b);
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("eded 3 reqemli deyil ");
+            //}
+            #endregion
+
+            #region Task 5
+            //Console.WriteLine("ededi daxil edin ");
+            //int i = int.Parse(Console.ReadLine());
+            //int reqemsayi = (int)Math.Log10(i) + 1;
+
+
+
+            //if (reqemsayi == 4)
+            //{
+            //    int result = 4 * (int)Math.Pow(10, reqemsayi + 2) + i * 100 + 44;
+            //    double a = result * 0.44;
+            //    Console.WriteLine(a);
+
+
+
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("4  reqemli eded deyil");
+
+            //}
+
+
+
+            #endregion
+
+            #region Task 6
+
+            //Console.WriteLine("ededi daxil edin ");
+            //int i = int.Parse(Console.ReadLine());
+            //int say = (int)Math.Log10(i) + 1;
+
+            //if (say == 4)
+            //{
+            //decimal a = i / 5;
+            //decimal b = a / 10;
+            //    decimal pow = (decimal)Math.Pow((double)b, 2);
+            //    Console.WriteLine($"{a}  {b}  {pow}");
+
+
+
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("eded 4 reqemli deyil");
+            //}
+            #endregion
+
+
+            #region  task 7 
+            //Console.WriteLine("ededi daxil edin ");
+            //int i = int.Parse(Console.ReadLine());
+            //int x = int.Parse(Console.ReadLine());
+            //int sayi = (int)Math.Log10(i) + 1;
+            //int sayx = (int)Math.Log10(x) + 1;
+
+            //if (sayi == 5 && sayx == 5)
+            //{
+            //    int a = i + x;
+            //    int reqem = (int)Math.Log10(a) + 1;
+            //    decimal result = 5 * (decimal)Math.Pow(10, reqem + 1) + a * 10 + 5;
+            //    decimal faiz = result / 20;
+
+            //    Console.WriteLine($" {a} , {result} , {faiz} ");
+
+
+
+
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("eded 5 reqemli deyil");
+            //}
+
+
+
+
+
+            #endregion
+
+            #region task 8
+
+            //Console.WriteLine("ededleri daxil edin ");
+            //int i = int.Parse(Console.ReadLine());
+            //int x = int.Parse(Console.ReadLine());
+            //int sayi = (int)Math.Log10(i) + 1;
+            //int sayx = (int)Math.Log10(x) + 1;
+
+            //if (sayi == 4 && sayx == 7)
+            //{
+            //    decimal a = i * 0.04m;
+            //    decimal b = x * 0.09m;
+            //    decimal c = (a + b) / 10;
+
+            //    Console.WriteLine($" {a} , {b} , {c} ");
+
+
+
+
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("ededler duzgun deyil");
+            //}
+
+
+
+            #endregion
+
+
+            #region task 9
+
+            //Console.WriteLine("ededleri daxil edin ");
+            //int i = int.Parse(Console.ReadLine());
+            //int x = int.Parse(Console.ReadLine());
+            //int y = int.Parse(Console.ReadLine());
+            //int say_i = (int)Math.Log10(i) + 1;
+            //int say_x = (int)Math.Log10(x) + 1;
+            //int say_y = (int)Math.Log10(y) + 1;
+
+
+            //if (say_i == 6 && say_x == 6 && say_y == 6)
+            //{
+            //    decimal a = i / 10;
+            //    decimal b = x / 10;
+            //    decimal c = y / 10;
+            //    decimal result = (a + b + c) / 10;
+
+            //    Console.WriteLine($" {a} , {b} , {c} , {result} ");
+
+
+
+
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("ededler duzgun deyil");
+            //}
+
+
+
+
+            #endregion
+
+            #region task 10 
+
+            //Console.WriteLine("ededleri daxil edin ");
+            //int i = int.Parse(Console.ReadLine());
+            //int x = int.Parse(Console.ReadLine());
+            //int y = int.Parse(Console.ReadLine());
+            //int say_i = (int)Math.Log10(i) + 1;
+            //int say_x = (int)Math.Log10(x) + 1;
+            //int say_y = (int)Math.Log10(y) + 1;
+
+
+            //if (say_i == 4 && say_x == 4 && say_y == 4)
+            //{
+            //    decimal a = i / 100;
+            //    decimal b = x / 50;
+            //    decimal c = y * 0.03m;
+            //    decimal ferq = a - b - c;
+            //    decimal result = (a - b - c) + (y * 7 / 100);
+
+            //    //    Console.WriteLine($" {a} , {b} , {c} ,  {ferq}  , {result} ");
+
+
+
+
+
+            //    //}
+            //    //else
+            //    //{
+            //    //    Console.WriteLine("ededler duzgun deyil");
+            //    //}
+
+
+            #endregion
+
+
+
+            #region task 11 
+
+            //    Label1:
+
+
+            //    Console.Write("a: ");
+
+            //    string stlA = Console.ReadLine();
+
+            //    bool veziyyetA = int.TryParse(stlA, out int a);
+            //    int countofDigitA = (int)Math.Log10(a) + 1;
+
+            //    if (false) 
+            //    {
+            //        goto Label1;
+
+
+            //    }
+            //    if (countofDigitA != 5)  
+            //    {
+            //        goto Label1;
+            //    }   
+
+            //    Label2:
+            //    Console.Write("b: ");
+            //    string stlB = Console.ReadLine();
+
+            //    bool veziyyetB = int.TryParse(stlB, out int b);
+            //    int countofDigitB = (int)Math.Log10(b) + 1;
+
+            //    if (false)
+            //    {
+            //        goto Label2;
+
+
+            //    }
+            //    if (countofDigitB != 5) 
+            //    {
+            //        goto Label2;
+            //    }
+            //Label3:
+            //    Console.Write("c: ");
+            //    string stlC = Console.ReadLine();
+
+            //    bool veziyyetC = int.TryParse(stlC, out int c);
+            //    int countofDigitC = (int)Math.Log10(c) + 1;
+
+            //    if (false)
+            //    {
+            //        goto Label3;
+
+
+            //    }
+            //    if (countofDigitC != 5)
+            //    {
+            //        goto Label3;
+            //    }
+            //Label4:
+            //    Console.Write("d: ");
+            //    string stlD = Console.ReadLine();
+
+            //    bool veziyyetD = int.TryParse(stlD, out int d);
+            //    int countofDigitD = (int)Math.Log10(d) + 1;
+
+            //    if (false)
+            //    {
+            //        goto Label4;
+
+
+            //    }
+            //    if (countofDigitD != 5)
+            //    {
+            //        goto Label4;
+            //    }
+
+            //    long StepFrst = a + c;
+            //    long StepScnd = b + d;
+            //    long FirstResult = StepFrst * StepScnd;
+            //    decimal LastResult = FirstResult - c * 0.03m;
+            //    Console.WriteLine($" {FirstResult} , {LastResult}");
+
+
+
+
+
+
+
+
+
+
+            #endregion
+        }
+    }
+}
